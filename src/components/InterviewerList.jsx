e7f1b0b230569582a2e9a1c3a9c800b306cdbd94
Youssef Nafaa <nafaayoussef@gmail.com>
2021-12-12 09:24:56 -0500

    refactor InterviewerList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,10 +1,24 @@
 
 import React from "react";
-export default function InterviewerListItem(props) {
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+	const interviewers = props.interviewers.map(interviewer => {
+		return (
+			<InterviewerListItem
+				key={interviewer.id}
+				name={interviewer.name}
+				avatar={interviewer.avatar}
+				selected={interviewer.id === props.value}
+				setInterviewer={() => props.onChange(interviewer.id)}
+			/>
+		);
+	});
 	return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
-    </section>
+		<section className="interviewers">
+			<h4 className="interviewers__header text--light">Interviewer</h4>
+			<ul className="interviewers__list">{interviewers}</ul>
+		</section>
 	);
 }
