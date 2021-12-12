4fda5bbf5495b8a41b7a3bc3b8b6d91c0ccea7ea
Youssef Nafaa <nafaayoussef@gmail.com>
2021-12-12 08:57:45 -0500

    add InterviewerList component style it and add storie of it

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,17 +1,20 @@
 
 import React from "react";
+import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
+	const interviewClass = classNames("interviewers__item", {
+		"interviewers__item--selected": props.selected
+	});
 
-return(
-  <li className="interviewers__item">
-    <img
-      className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
-    />
-    Sylvia Palmer
-  </li>
-);
+	return (
+		<li className={interviewClass} onClick={props.setInterviewer}>
+			<img
+				className="interviewers__item-image"
+				src={props.avatar}
+				alt={props.name}
+			/>
+			{props.selected && props.name}
+		</li>
+	);
 }
-
