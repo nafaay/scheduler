05a1fa95c7c38e73ff52ad9b32848d3c6b95b90a
Youssef Nafaa <nafaayoussef@gmail.com>
2021-12-14 12:05:06 -0500

    refactor application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,109 +1,61 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import DayList from './DayList';
-import InterviewerList from './InterviewerList';
-
+import React from "react";
 
 import "components/Application.scss";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from '../hooks/useApplicationData'
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
+export default function Application() {
+
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+
+  /* build schedule */
+
+  const schedule = getAppointmentsForDay(state, state.day).map(appointment => {
+
+    const passProps = {
+      ...appointment,
+      key: appointment.id,
+      interview: getInterview(state, appointment.interview),
+      interviewers: getInterviewersForDay(state, state.day),
+      bookInterview,
+      cancelInterview
     }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
 
-
-
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
-
-export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    const day = [{
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    ];
-    setDay(day);
-  }, [])
-
-   
-  useEffect(() => {
-    const days = [
-      {
-        id: 1,
-        name: "Monday",
-        spots: 2,
-      },
-      {
-        id: 2,
-        name: "Tuesday",
-        spots: 5,
-      },
-      {
-        id: 3,
-        name: "Wednesday",
-        spots: 0,
-      },
-    ];    
-    setDays(days);
-  }, [])
+    return <Appointment {...passProps} />;
+  });
 
   return (
     <main className="layout">
       <section className="sidebar">
-        <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
-        </nav>
-        <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
+          <DayList
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
+          /></nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* <InterviewerList interviewers={interviewers} interviewer={interviewer} setInterviewer={setInterviewer} /> */}
+        {schedule}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
-
